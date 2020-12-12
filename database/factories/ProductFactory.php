67d31dd235e1a4b508694e3bbd2871e997a6dd7f
cbl <lennart.carbe@gmail.com>
2020-12-12 13:56:32 +0100

    wip

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Model;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Model::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name'       => $this->faker->company,
+            'desciption' => $this->faker->sentence($nbWords = 6, $variableNbWords = true),
+            'price'      => $this->faker->randomFloat($nbMaxDecimals = 2, $min = 10, $max = 100),
+        ];
+    }
+}
